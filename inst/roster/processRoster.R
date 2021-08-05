61a88eeafb28da3e76d3685a1a3ddd19f757153d
Jean-Philippe Fortin <fortin.jeanphilippe@gene.com>
2021-08-05 13:41:21 -0700

    Updated roster

--- inst/roster/processRoster.R
+++ inst/roster/processRoster.R
@@ -1,4 +1,7 @@
-roster <- read.table("methods.txt", head=TRUE, stringsAsFactors=FALSE)
+roster <- read.table("methods.txt",
+                     head=TRUE,
+                     stringsAsFactors=FALSE)
 roster$len <- roster$right-roster$left+1
 scoringMethodsInfo <- roster
-save(scoringMethodsInfo, file="../../data/scoringMethodsInfo.rda")
+save(scoringMethodsInfo,
+     file="../../data/scoringMethodsInfo.rda")
