7ce56e6d3b78dceeaa34ab8a87672b689a38b641
developerpiru <pirunthan.perampalam@contractors.roche.com>
2021-08-17 02:58:27 -0400

    add more dependencies

--- R/basilisk_min.R
+++ R/basilisk_min.R
@@ -1,14 +1,18 @@
 crisprai_dependencies <- c("python==2.7.15",
                            "pip==20.1.1",
                            "wheel==0.36.2",
-                           "setuptools==49.6.0",
+                           "setuptools==44.0.0",
                            "numpy==1.11.3",
                            "scipy==0.18.1",
                            "biopython==1.68",
                            "pysam=0.15.3",
                            "viennarna=2.4.18",
                            "scikit-learn==0.17.1",
-                           "bx-python==0.7.3")
+                           "bx-python==0.7.3",
+                           "libgomp==9.3.0",
+                           "libgfortran5==9.3.0",
+                           "libedit==3.1.20191231",
+                           "sqlite==3.35.5")
 
 crisprai_dependencies_pip <- c("pandas==0.15.0")
 
