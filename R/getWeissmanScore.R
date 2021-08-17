8e37508518aac5c0c28cba0e6ae3a6c1a2f6c1dc
developerpiru <pirunthan.perampalam@contractors.roche.com>
2021-08-16 23:27:43 -0400

    force python 2.7.15

--- R/getWeissmanScore.R
+++ R/getWeissmanScore.R
@@ -9,7 +9,7 @@
 library(reticulate)
 
 use_condaenv(condaenv="crisprai2", required=TRUE)
-# use_python_version(version="2.7", required = FALSE)
+use_python_version(version="2.7.15", required = FALSE)
 
 # basilisk::configureBasiliskEnv(src = "/gstore/data/omni/crispr/piru/crisprScore/R/basilisk_min.R")
 
