d4722381aff4e461650480bf9821095f4aeb0eea
fortinj2 <fortin.jeanphilippe@gene.com>
2022-07-22 11:31:05 -0700

    Added isntrution

--- inst/casrxrf/binaryInstall.sh
+++ inst/casrxrf/binaryInstall.sh
@@ -4,7 +4,19 @@
 # - viennarna/bin/RNAplfold
 # - RNAhybrid/bin/RNAhybrid
 
-# For RNAhybrid, I compiled the code from source using 
+# For RNAhybrid, I downloaded the source code from
+# https://bibiserv.cebitec.uni-bielefeld.de/rnahybrid/
+#
+# I unzipped the folder, entered the folder, and compiled the code from source using 
 # ./configure --prefix=/gstore/data/omni/crispr/programs/RNAhybrid-2.1.2
+# make
+# make install
+
+
+# For ViennaRNA, I downlaoded the source code from their website
+# https://www.tbi.univie.ac.at/RNA/
+
+# I unzipped the folder, entered the folder, and compiled the code from source using 
+# ./configure --prefix=/gstore/data/omni/crispr/programs/ViennaRNA-2.4.9 --without-gsl
 # make
 # make install
