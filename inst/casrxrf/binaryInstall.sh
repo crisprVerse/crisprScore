ed7799d29f4fcdc85d153363eeaa4f840a80da99
fortinj2 <fortin.jeanphilippe@gene.com>
2022-07-22 08:47:00 -0700

    Installed note

---
+++ inst/casrxrf/binaryInstall.sh
@@ -0,0 +1,10 @@
+# The ViennaRNA and RNAhybrid packages must be installed
+# so that the following binaries are available to crisprScore:
+# - viennarna/bin/RNAfold
+# - viennarna/bin/RNAplfold
+# - RNAhybrid/bin/RNAhybrid
+
+# For RNAhybrid, I compiled the code from source using 
+# ./configure --prefix=/gstore/data/omni/crispr/programs/RNAhybrid-2.1.2
+# make
+# make install
