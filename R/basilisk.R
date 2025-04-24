d255647ee6cafefad09322c8ffaf935d87f03d26
mzhibo <32151277+mzhibo@users.noreply.github.com>
2025-04-24 12:34:11 -0700

    Update basilisk.R on conda channel priority for RuleSet3 env setup
    conda-forge ahead of bioconda to avoid biopython installation error due to strict repo priority

--- R/basilisk.R
+++ R/basilisk.R
@@ -109,7 +109,7 @@ env_deepspcas9 <- BasiliskEnvironment(envname="deepspcas9__basilisk",
 env_rs3 <- BasiliskEnvironment(envname="rs3__basilisk",
                                pkgname="crisprScore",
                                packages=rs3_dependencies,
-                               channels = c("bioconda", "conda-forge"),
+                               channels = c("conda-forge","bioconda"),
                                pip=rs3_dependencies_pip)
 
 if (.Platform$OS.type!="windows"){
