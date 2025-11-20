2643bc2f9d9176a7d08654e0d1cb05745eb9de68
Jean-Philippe Fortin <fortin946@gmail.com>
2025-11-19 23:43:42 -0500

    Updated basilisk

--- R/basilisk.R
+++ R/basilisk.R
@@ -2,17 +2,3 @@ lindel_dependencies <- c("python==3.6",
                          "numpy==1.19.5",
                          "pip==21.0.1",
                          "scipy==1.5.3")
-
-enpamgb_dependencies <- c("python==3.6",
-                          "biopython==1.78",
-                          "h5py==2.10.0",
-                          "Keras-Preprocessing==1.1.2",
-                          "numpy==1.19.5",
-                          "pandas==1.1.5",
-                          "pip==21.0.1",
-                          "scikit-learn==0.21.2",
-                          "scipy==1.5.3",
-                          "setuptools==49.6.0",
-                          "six==1.15.0")
-enpamgb_dependencies_pip <- c("tensorflow==2.4.1")
-
